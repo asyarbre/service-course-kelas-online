2dc0ab10bf3811bc817bc56d4f65d55dd6dc7144
Asya'ri ‎ <asyari818@gmail.com>
2023-02-01 20:19:31 +0700

    [Course] - API Update

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -53,4 +53,63 @@ class CourseController extends Controller
             'data' => $course,
         ]);
     }
+
+    public function update(Request $request, $id)
+    {
+        $rules = [
+            'name' => 'string',
+            'certificate' => 'boolean',
+            'thumbnail' => 'url',
+            'type' => 'in:free,premium',
+            'status' => 'in:draft,published',
+            'price' => 'integer',
+            'level' => 'in:all-level,beginner,intermediate,advanced',
+            'mentor_id' => 'integer',
+            'description' => 'string',
+        ];
+
+        $data = $request->all();
+
+        $validator = Validator::make($data, $rules);
+
+        // If validation fails, return error response
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 'error',
+                'message' => $validator->errors(),
+            ], 400);
+        }
+
+        // Find course by id
+        $course = Course::find($id);
+        if (!$course) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Course not found',
+            ], 404);
+        }
+
+        // check if mentor_id is provided
+        $mentor_id = $request->input('mentor_id');
+        if ($mentor_id) {
+            $mentor = Mentor::find($mentor_id);
+
+            // If mentor not found, return error response
+            if (!$mentor) {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'Mentor not found',
+                ], 404);
+            }
+        }
+
+        // Update course
+        $course->fill($data);
+        $course->save();
+
+        return response()->json([
+            'status' => 'success',
+            'data' => $course,
+        ]);
+    }
 }
