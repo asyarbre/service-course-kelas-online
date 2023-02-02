0c850dd7336050303aa973dd66ac4537e4f4e7d3
Asya'ri ‎ <asyari818@gmail.com>
2023-02-02 19:16:27 +0700

    Remove / in url variable

--- app/helpers.php
+++ app/helpers.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Http;
 // this function is used to get user data from service-user
 function getUser($userId)
 {
-  $url = env('SERVICE_USER_URL') . '/users/' . $userId;
+  $url = env('SERVICE_USER_URL') . 'users/' . $userId;
 
   try {
     $response = Http::timeout(10)->get($url);
@@ -24,7 +24,7 @@ function getUser($userId)
 // this function is used to get user data from service-user like getUser() but with multiple user id
 function getUserByIds($userIds = [])
 {
-  $url = env('SERVICE_USER_URL') . '/users/';
+  $url = env('SERVICE_USER_URL') . 'users/';
 
   try {
     if (count($userIds) === 0) {
