3732708f55ea101f14a5ef1bfffc85eba7b2b801
Asya'ri ‎ <asyari818@gmail.com>
2023-02-02 14:06:29 +0700

    [Lesson] - API Update

--- app/Http/Controllers/LessonController.php
+++ app/Http/Controllers/LessonController.php
@@ -48,4 +48,55 @@ class LessonController extends Controller
             'data' => $lesson,
         ]);
     }
+
+    public function update(Request $request, $id)
+    {
+        $rules = [
+            'name' => 'string',
+            'video' => 'string',
+            'chapter_id' => 'integer',
+        ];
+
+        $data = $request->all();
+
+        $validator = Validator::make($data, $rules);
+
+        // If validation fails, return error response
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 'error',
+                'message' => $validator->errors(),
+            ], 400);
+        }
+
+        // check if lesson exists
+        $lesson = Lesson::find($id);
+        if (!$lesson) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'lesson not found',
+            ], 404);
+        }
+
+        // check if chapter exists
+        $chapterId = $request->input('chapter_id');
+        if ($chapterId) {
+            $chapter = Chapter::find($chapterId);
+            if (!$chapter) {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'chapter not found',
+                ], 404);
+            }
+        }
+
+        // update lesson
+        $lesson->fill($data);
+        $lesson->save();
+
+        return response()->json([
+            'status' => 'success',
+            'data' => $lesson,
+        ]);
+    }
 }
